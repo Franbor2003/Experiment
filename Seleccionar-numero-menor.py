02f278f0aa059fcd69542cc49fc28a24b53a1449
Francisco Javier <144556857+Franbor2003@users.noreply.github.com>
2023-09-10 00:07:04 +0200

    Add files via upload

---
+++ Seleccionar-numero-menor.py
@@ -0,0 +1,9 @@
+print ("Programa que selecciona el numero menor introducido")
+print ("===================================================")
+numero1 = int(input("Ingrese el primer número: "))
+numero2 = int(input("Ingrese el segundo número: "))
+numero3 = int(input("Ingrese el tercer número: "))
+
+minimo = min(numero1, numero2, numero3)
+
+print("El mínimo es el", minimo)
